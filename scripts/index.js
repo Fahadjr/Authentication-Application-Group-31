3311b5a41aae9d5714d62a5a1b1437629b190b3e
mahanth-windsor <98180683+mahanth-windsor@users.noreply.github.com>
2022-09-25 16:10:21 -0400

    Image upload and access control
    Code for image upload module,
    Component access to logged in and loggedout users

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,46 @@
+
+const guideList = document.querySelector('.guides');
+
+const loggedOutElements = document.querySelectorAll('.logged-out');
+const loggedInElements = document.querySelectorAll('.logged-in');
+
+const setupUi = (user) => {
+  if(user){
+    loggedInElements.forEach(ele => ele.style.display = 'block');
+    loggedOutElements.forEach(ele => ele.style.display = 'none');
+  }else{
+    loggedInElements.forEach(ele => ele.style.display = 'none');
+    loggedOutElements.forEach(ele => ele.style.display = 'block');
+  }
+}
+
+const setupGuides = (data) => {
+
+  let html = '';
+
+  if(data.length){
+    data.forEach(doc => {
+      const profile = doc.data();
+      console.log(profile);
+      const li = `
+        <li>
+          <div class="collapsible-header grey lighten-4">${profile.bio}</div>
+          <div class="collapsible-body white"><span>${profile.imageUrl}</span></div>
+          <div class="collapsible-body white"><span>${profile.imageName}</span></div>
+          <img class="collapsible-body white" id="photo" src="${profile.imageUrl}" style="width: 100%"/>
+        </li>
+      `;
+  
+      html += li;
+    });
+  
+    guideList.innerHTML = html; 
+  }else{
+    guideList.innerHTML = `<h5 class="center-align">Login to view data</h5>`; 
+  }
+  
+}
+
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
 
